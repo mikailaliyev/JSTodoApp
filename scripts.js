f7af1c72c8bdd12a4b2ad1285a904759ac4afa58
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-19 23:57:26 +0400

    testing sample added todo area

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
-const inputText = document.getElementById("inputText")
-const todosArea = document.getElementById("todos")
-const addBtn = document.getElementById("addBtn")
+const inputText = document.getElementById("inputText");
+const todosArea = document.getElementById("todos");
+const addBtn = document.getElementById("addBtn");
+const checkBox = document.getElementById("checkbox");
+const testText = document.getElementById("test");
 
-addBtn.addEventListener('click', ()=> {
-    const paragraph = document.createElement("p")
-    paragraph.innerText = inputText.value
-    todosArea.appendChild(paragraph)
+addBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const paragraph = document.createElement("p");
+  if (inputText.value) {
+    paragraph.innerText = inputText.value;
+    todosArea.appendChild(paragraph);
+  }
+});
 
-})
+checkBox.addEventListener("click", () => {
+  console.log(checkBox.checked);
+  if (checkBox.checked) {
+    testText.style.textDecoration = "line-through";
+  } else {
+    testText.style.textDecoration = "none";
+  }
+});
